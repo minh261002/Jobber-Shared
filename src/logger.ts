6ad33e22eef87d40755508adae08f45015fc4789
minhttran <trcongminh.261002@gmail.com>
2024-08-21 13:59:07 +0700

    Configure and github actions

---
+++ src/logger.ts
@@ -0,0 +1,37 @@
+import winston, {log, Logger} from 'winston';
+import {ElasticsearchTransformer, ElasticsearchTransport, LogData, TransformedData} from 'winston-elasticsearch'
+
+const esTransformer = (logData: LogData): TransformedData => {
+  return ElasticsearchTransformer(logData);
+}
+
+export const winstonLogger = (elasticsearchNode: string,name:string, level: string):Logger => {
+  const options = {
+    console: {
+      level,
+      handleExceptions: true,
+      json: false,
+      colorize: true
+    },
+    elasticsearch: {
+      level,
+      trnasformer: esTransformer,
+      clientOpts: {
+        node: elasticsearchNode,
+        log: level,
+        maxRetries: 2,
+        requestTimeout: 10000,
+        sniffOnStart: false
+      }
+    }
+  }
+
+  const eTransport:ElasticsearchTransport = new ElasticsearchTransport(options.elasticsearch);
+  const logger:Logger = winston.createLogger({
+    exitOnError:false,
+    defaultMeta: {service: name},
+    transports: [new winston.transports.Console(options.console), eTransport]
+  });
+
+  return logger;
+}
